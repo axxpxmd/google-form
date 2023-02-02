7c017907c1e158cc3dbab5399bef8df4a1a0390f
Asip Hamdi <asiphamdi@gmail.com>
2023-02-02 11:19:39 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Generate</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        a {
+            color: black;
+        }
+
+        a:active {
+            color: blue;
+        }
+
+        a[tabindex]:focus {
+            color: blue;
+            outline: none;
+        }
+    </style>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container mt-4 col-md-8">
         <div class="card">
@@ -15,7 +33,7 @@
                 <ol>
                     @foreach ($datas as $i)
                         <li class="mb-3">
-                            <a href="{{ $i }}" target="blank" class="text-primary">{{ $i }}</a>
+                            <a href="{{ $i }}" tabindex="1" target="blank" class="text-primary">{{ $i }}</a>
                         </li>
                     @endforeach
                 </ol>
@@ -23,4 +41,5 @@
         </div>
     </div>
 </body>
+
 </html>
